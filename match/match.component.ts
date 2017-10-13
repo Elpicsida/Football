f4a45d67d8561f0c6077702d343f84e32be975c7
Kava <elpicsida@gmail.com>
2017-10-14 00:39:50 +0200

    Working cup stage

--- match/match.component.ts
+++ match/match.component.ts
@@ -11,14 +11,48 @@ export class MatchComponent implements OnInit {
 
   @Input() HomeTeam: Team;
   @Input() AwayTeam: Team;
+  @Input() Name : string;
+  HomeGoals? : number;
+  AwayGoals? : number;
+  HomePenaltyGoals? :number;
+  AwayPenaltyGoals? :number;
+  Winner : Team;
+
+  match : Match;
 
   public IsPlayed() {
   } 
 
   constructor() { 
+    
+  }
+
+  checkWinner() : void {
+    if (this.HomePenaltyGoals != null && this.AwayPenaltyGoals != null) {
+      this.setWinner(this.HomePenaltyGoals > this.AwayPenaltyGoals ? this.HomeTeam : this.AwayTeam);
+    }
   }
 
   ngOnInit() {
+    this.match = new Match(this.HomeTeam, this.AwayTeam, this.Name);
+    this.Winner = new Team('Winner of ' + this.Name, "/assets/flags/Unknown.png", 1);
+  }
+
+  showPenaltyBlock():boolean {
+    if (this.HomeGoals != null && this.AwayGoals != null) {
+      if (this.AwayGoals === this.HomeGoals) {
+        return true;
+      }
+      else {
+        this.setWinner(this.AwayGoals > this.HomeGoals ? this.AwayTeam : this.HomeTeam);
+        return false;
+      }
+    }
+    return false;
+  }
+
+  setWinner(team : Team) {
+    this.Winner = team;
   }
 
 }
@@ -29,10 +63,15 @@ export class Match {
    AwayTeam : Team;
    HomeGoals? : number;
    AwayGoals? : number;
+   PenaltyHome? : number;
+   PenaltyAway? : number;
 
-   constructor(homeTeam : Team, awayTeam : Team) {
+   Winner : Team;
+
+   constructor(homeTeam : Team, awayTeam : Team, name? :string) {
     this.Name = "Match";
     this.HomeTeam = homeTeam;
     this.AwayTeam = awayTeam;
+    this.Winner = new Team('', "/assets/flags/Unknown.png", 1);
   }
 }
